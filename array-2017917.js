b78fc1224fd818aacabf4e1927bba507948af842
刘小妞 <14120329@bjtu.edu.cn>
2017-09-18 00:11:02 +0800

    Create array-2017917.js
    first day to selfStudy

---
+++ array-2017917.js
@@ -0,0 +1,39 @@
+//创建数组
+let fruits = ["apple", "banana"];
+
+//访问元素
+let first = fruits[0];
+
+//遍历一个数组
+
+fruits.forEach(function(item, index, array) {
+  console.log(item, index);
+})
+
+//添加元素到末尾
+var newLength = fruits.push("orange");
+
+//删除数组末尾的元素
+let last  = fruits.pop();
+
+//删除数组最前面的元素
+let first = fruits.shift();
+
+//添加到数组的前面
+let newLength  = fruits.unshift("strawberry")
+
+//找到元素的索引
+let index = fruits.indexOf("banana")
+
+//通过索引删除某个元素
+let removedIndex = fruits.splice(pos, 1);
+
+//从某个索引开始，删除多个元素
+
+let removedItems = vegetables.splice(pos, n);
+
+//复制一个数组
+var shallowCopy = fruits.slice();
+
+
+
